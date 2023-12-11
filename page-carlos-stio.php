b0e61e271ba7fd07d1793bb5e29bc58ecee75cd8
Diego Rodríguez <dieguitotoi@gmail.com>
2023-12-11 05:00:14 -0500

    Memorial

--- page-carlos-stio.php
+++ page-carlos-stio.php
@@ -4,7 +4,7 @@
  */
 get_header('carlos-sitio'); ?>
 		<section id="contenido" class="pc-centrad-supergrandeCS">
-        <img src="<?php echo get_template_directory_uri(); ?>/imagenes/fondos/CarlosSitio.png" alt="Carlos Sitio" id="carlosSitio">
+        <img src="<?php echo get_template_directory_uri(); ?>/imagenes/fondos/CarlosSitio.png" alt="Carlos Stio" id="carlosSitio">
 			<h1><?php the_title();?></h1>
             <div class="pc-centrado-extraGrande" style="margin-bottom: 100px!important;">
                  <?php 

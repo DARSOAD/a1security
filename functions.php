d22b5db4bf9be7aa138980314218b0fe16a54133
Diego Rodríguez <dieguitotoi@gmail.com>
2023-12-19 22:07:29 -0500

    Intento 1 de imagen en functios clients

--- functions.php
+++ functions.php
@@ -13,5 +13,26 @@ function get_excerpt($limit, $source = null){
 }
 add_post_type_support('page', 'excerpt'); 
 
+function add_thumbnail_support_for_clients() {
+    add_theme_support('post-thumbnails', array('Clients'));
+}
+add_action('after_setup_theme', 'add_thumbnail_support_for_clients');
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+	$args = array(
+	    'labels' => array(
+            'name' => __( 'Clients' ),
+            'singular_name' => __( 'Client' )
+        ),
+        'public' => true,
+        'has_archive' => true,
+		'menu_icon'           => 'dashicons-testimonial', // string
+        'supports' => array('title', 'editor', 'thumbnail') // Añadir soporte para miniaturas
+	);
+
+	/* Register the post type. */
+	register_post_type('Clients', $args);
+}
 
 ?>

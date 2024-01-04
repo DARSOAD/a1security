116968c73792b1416c6cee022fc241c751a4fdb6
Diego Rodríguez <dieguitotoi@gmail.com>
2024-01-03 21:59:04 -0500

    Página de clientes terminada

--- js/menu_pc.js
+++ js/menu_pc.js
@@ -21,6 +21,7 @@ jQuery(document).ready(function($) {
         imagen.setAttribute('src', originalSrc);
         imagen.setAttribute('srcset', originalSrcset);
         tituloH1.style.display  = 'block'; 
+        console.log(parrafo);
         parrafo.style.color  = 'white'; 
 
     });
